fd236fc31cc786fe066cc04cc018a7c30c59c189
Prithvi01745 <ac5714043@gmail.com>
2026-07-31 00:03:38 +0530

    Fix AuthContext import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css"; // ✅ This line is required
 
 import App from "./App";
-import AuthProvider from "./context/AuthContext";
+import AuthProvider from "./context/Authcontext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
